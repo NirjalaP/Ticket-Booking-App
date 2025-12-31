c76662993a388c167b88d3b093daae5090ab1867
NirjalaP <nirjala.prajapati11@gmail.com>
2025-12-31 07:43:50 -0800

    Organize pages and components and update App routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin'); 
   return (
-    <div>App</div>
+    <>
+    {!isAdminRoute && <Navbar />}
+    <Routes>
+      <Route path = '/' element = {<Home/>} />
+      <Route path = '/movies' element = {<Movies/>} />
+      <Route path = '/movies/:id' element = {<MovieDetails/>} />
+      <Route path = '/movies/:id/:date' element = {<SeatLayout/>} />
+      <Route path = '/my-bookings' element = {<MyBookings/>} />
+      <Route path = '/favourite' element = {<Favourite/>} />
+    </Routes>
+    </>
   )
 }
 
