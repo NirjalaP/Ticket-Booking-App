fefef9c7113025e1e0314a063f12a96ac5930f4b
NirjalaP <nirjala.prajapati11@gmail.com>
2026-01-05 06:51:33 -0800

    Add MovieDetails page with cast list, date selector, and booking navigation

---
+++ client/src/pages/Home copy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailersSection from '../components/TrailersSection'
+import Test from '../components/Test'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+   <>
+    <HeroSection />
+    <FeaturedSection />
+    <TrailersSection />
+   
+   </>
+  )
+}
+
+export default Home
--- client/src/pages/Movies copy.jsx
+++ client/src/pages/Movies copy.jsx
@@ -3,7 +3,7 @@ import { dummyShowsData } from '../assets/assets'
 import MovieCard from '../components/MovieCard'
 import BlurCircle from '../components/BlurCircle'
 
-const Favourite = () => {
+const Movies = () => {
   return dummyShowsData.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h[80vh]'>
 
@@ -11,7 +11,7 @@ const Favourite = () => {
     <BlurCircle bottom="50px" right="50px" />
 
 
-      <h1 className='text-lg font-medium my-4'>Your Favourite Movies</h1>
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
       <div className='mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 place-items-center'>
         {dummyShowsData.map((movie) => (
           <MovieCard  movie = {movie} key = {movie._id}/>
@@ -25,4 +25,4 @@ const Favourite = () => {
   )
 }
 
-export default Favourite
+export default Movies
