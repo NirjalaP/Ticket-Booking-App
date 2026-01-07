5eb63b3ac20ebf7b66070f93d249d857464071f6
NirjalaP <nirjala.prajapati11@gmail.com>
2026-01-07 09:58:17 -0800

    Add user create, update, and delete event handlers

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
         mongoose.connection.on('connected', ()=> console.log('Database connected'))
         await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
     } catch (error){
-        console.llog(error.message)
+        console.log(error.message)
     }
 }
 
